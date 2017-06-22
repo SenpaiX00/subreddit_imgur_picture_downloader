31ac914e6549f4901dc2f7d02d0fd7911afb7499
ccodingsKILLS <augustus.writer@gmail.com>
2017-06-22 23:58:46 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import praw
+username = "USERNAME GOES HERE"
+password = "SPASSWORD GOES HERE"
+client_id = "ID GOES HERE"
+client_secret = "SECRET GOES HERE"
+
+#Don't know what should go in here? Then watch this tut: https://www.youtube.com/watch?v=krTUf7BpTc0
